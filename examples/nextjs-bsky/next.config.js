bc6f4d5fc4b43e9dceb048e793fb53f636e77d05
Vordgi <sasha2822222@gmail.com>
2025-11-27 16:54:17 +0000

    issue-72 use redis handler only in nextjs-bsky prod

--- examples/nextjs-bsky/next.config.js
+++ examples/nextjs-bsky/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   // you can remove these lines and the cache-handlers directory
   cacheHandlers: {
     remote:
-      process.env.REDIS_URL
+      process.env.REDIS_URL && process.env.REDIS_STORE === "true"
         ? require.resolve("./cache-handlers/redis.js")
         : undefined,
   },
